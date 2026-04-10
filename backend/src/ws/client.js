af1c58f272c1c2f1ead8774737ad303798bd9fba
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-04-10 17:51:59 +0530

    refactor: decouple WebSocket client from server creation logic

---
+++ backend/src/ws/client.js
@@ -0,0 +1,36 @@
+import { WebSocket } from "ws";
+import sendJson from "../helper/sendJson";
+import { rooms } from "../services/room";
+
+export default function createWebSocketClient(){
+    const ws = new WebSocket(process.env.RELAY_URL);
+
+    return new Promise((resolve, reject) => {
+        ws.onopen = ()=> {
+            console.log("Connected to relay server");
+
+            ws.onmessage = ({data})=>{
+                let parsedMessage;
+                try {
+                    parsedMessage = JSON.parse(data);
+                } catch (error) {
+                    console.error("JSON parse error:", error);
+                    return ;
+                }
+
+                if(parsedMessage.type === "message_relayed"){
+                    const {message, roomId} = parsedMessage.payload;
+                    const existingUsers = rooms.get(roomId)?.users;
+                    if(existingUsers && existingUsers.size > 0){
+                        for(const [user, userSocket] of existingUsers){
+                            sendJson(userSocket, {type: "new_message", payload: { message, roomId }});
+                        }
+                    }
+                }
+            }
+            resolve(ws);
+
+        }
+        ws.onerror = (err)=> reject(err);
+    })
+}
